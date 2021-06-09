a8b25ec81ca8b9891e2181d461de5f168d98c88a
Erica Rosalina <erica@ailo.io>
2021-06-09 16:51:04 +1000

    fix: Remove Logger.log to make it match with @ailo/logger

--- src/LoggerInterface.ts
+++ src/LoggerInterface.ts
@@ -4,6 +4,5 @@ export interface LoggerInterface {
   debug(...data: any[]): void;
   error(...data: any[]): void;
   info(...data: any[]): void;
-  log(...data: any[]): void;
   warn(...data: any[]): void;
 }
